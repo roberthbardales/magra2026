f63de97ff700b25861216913533c2fdc20f6d361
unknown <roberthbardales@hotmail.com>
2026-03-07 19:34:36 -0500

    estructura base

---
+++ magra2026/applications/home/urls.py
@@ -0,0 +1,13 @@
+#
+from django.urls import path
+from . import views
+
+app_name = "home_app"
+
+urlpatterns = [
+    # path(
+    #     'register/',
+    #     views.UserRegisterView.as_view(),
+    #     name='user-register',
+    # ),
+]
